d8169595f7ee2bc950d8e87d9ec28b9619cb8671
beni-opportunity <beni@opportunity-zuerich.ch>
2024-03-25 13:59:48 +0100

    removed test route in vue

--- resources/js/router.js
+++ resources/js/router.js
@@ -10,11 +10,6 @@ export const routes = [
         path: "/login",
         component: () => import("./pages/Auth/Login.vue"),
     },
-    {
-        path: "/test",
-        component: () => import("./pages/Test.vue"),
-        meta: { requiresAuth: true },
-    },
 
     {
         path: "/dashboard",
